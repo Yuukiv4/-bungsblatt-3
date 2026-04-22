b0ff089a876974cef042cb2caef96e3c468ccd7e
Yuukiv4 <Knight169@gmx.net>
2026-04-22 12:46:36 +0200

    final
    Erstellung der Methode für die Quersummen berechnung

--- Aufgabe_1/src/Main.java
+++ Aufgabe_1/src/Main.java
@@ -2,11 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Erstellung des Scanners
         Scanner input = new Scanner(System.in);
+        //Eingabe der gewünschten Zahl
         System.out.println("Bitte geben sie ihr Zahl ein, beachten sie, das die Zahl nicht größer sein darf als 999");
         int zahl = input.nextInt();
+        System.out.println("Bitte geben sie die Basis ihres gewünschten Stellenwertsystems ein");
+        int basis = input.nextInt();
+        System.out.println("Bitte geben sie ihre Zahl,welche in ihrem Stellewertsystem liegt, ein, sie darf nur dreistellig sein");
+        String hexZahl = input.next();
         input.close();
-        System.out.println("Die Quersumem ihrer Zahl lautet: " + Quersumme.quersumme(zahl));
+        System.out.println(hexZahl);
+
+
+        //Aufruf der class "Quersumme" und der Funktion Quersumme in dieser class mit der übergabe des Integers zahl
+        int ergebnissZahl = Quersumme.quersumme(zahl);
+        //Aufruf der class "Quersumme" und der Funktion Quersumme in dieser class mit der Übergabe des Strings hexZahl
+        String ergebnissHex = Quersumme.quersummeHex(hexZahl,basis);
+        //Ausgabe der Quersumme der Zahl
+        System.out.println("Die Quersumme ihrer Zahl lautet: " + ergebnissZahl);
+        //Ausgabe der Zahl, welche in einem beliebigen Stellenwertsysstem eingegeben wurde und als Hexadezimal ausgegeben wird
+        System.out.println("Die Quersumme ihrer Zahl innerhalb des Stellenwertsystems mit der Basis " + basis + " lautet umgewandelt in eine Hexadezimal Zahl: " + ergebnissHex);
 
 
     }
--- Aufgabe_1/src/Quersumme.java
+++ Aufgabe_1/src/Quersumme.java
@@ -3,25 +3,52 @@ public class Quersumme {
     public static int quersumme(int x) {
         int quersumme = 0;
         while (x != 0) {
+            //Es wird darauf geprüft, ob eine andere zahl als 0 auf der Einser-Stelle liegt
             if (x % 10 != 0) {
+                //Die Einer werden aus der gesamten Zahl gefiltert
                 int ones = x % 10;
+                //Die Einer werden von der gesamten Zahl abgezogen
                 x -= ones;
+                //Die Einer werden auf die Quersumme addiert
                 quersumme += ones;
+            //Es wird darauf geprüft, ob eine andere zahl als 0 auf der Zehner-Stelle liegt
             }else if (x % 100 != 0) {
-                int tens = (x % 100) / 10;
+                //Die Zehner werden aus der gesamten Zahl gefiltert
+                int tens = (x % 100);
+                //Die Zehner werden von der gesamten Zahl abgezogen
                 x -= tens;
-                quersumme += tens;
+                //Die Zehner werden auf die Quersumme addiert
+                quersumme += tens / 10;
+            //Es wird darauf geprüft, ob eine andere zahl als 0 auf der Hunderter-Stelle liegt
             } else if (x % 1000 != 0) {
-                int hundreds = (x % 1000) / 100;
-                quersumme += hundreds;
+                //Die Hunderter werden aus der gesamten Zahl gefiltert
+                int hundreds = (x % 1000);
+                //Die Hunderter werden von der gesamten Zahl abgezogen
+                x -= hundreds;
+                //Die Hunderter werden auf die Quersumme addiert
+                quersumme += hundreds / 100;
             }
-
         }
 
         return quersumme;
     }
 
-    public static int quersummeHex (int x) {
+    public static String quersummeHex (String number, int basis) {
+        //i ist die Zähler Variable wie oft die Schleife schon durchgelaufen ist
+        int i = 0;
+        int quersumme = 0;
+        //Schleife die so lange läuft bis jede Stelle der Zahl verarbeitet wurde
+        while ( i < number.length() ) {
+            //Aufteilung der Zahl in die einzelnen Stellen
+            String position = number.substring(i,i+1);
+            //Umwandlung der Zahl in Ihrem Stellenwertsystem zu einer Zahl und addition zur Quersumme
+            quersumme += Integer.parseInt(position,basis);
+            //erhöhung der Zähler Variable um 1
+            i += 1;
+        }
+        //Dieser Ansatz in die Aufteilung der zahl in Substrings, würde auch beim Verfahren in der Funktion quersumme funktionieren, wenn dort die Zahl vorherr als String eingelesen wird statt als Integer
+
+        return (Integer.toHexString(quersumme));
 
     }
 
